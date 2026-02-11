cf604a5be3016c1856f1835e09e768d3246980db
josephteh97 <1079183153@qq.com>
2026-02-11 09:35:22 +0800

    adjust the port to correct one

--- backend/app.py
+++ backend/app.py
@@ -92,7 +92,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "app:app",
         host="0.0.0.0",
-        port=8000,
+        port=5173,
         reload=True,
         log_level="info"
     )
